394de14519fd5a44d47ac526bba622c825543ae9
Oliver Caldwell <olliec87@gmail.com>
2013-11-13 22:48:35 +0000

    More JavaScript dependencies and a "Hello, World!"
    I have a basic template rendering over AMD with auto complication. And
    it's saying hello to me :D
    
    So much win.

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+require.config({
+	paths: {
+		mustache: '../../bower_components/mustache/mustache',
+		text: '../../bower_components/requirejs-text/text',
+		stache: '../../bower_components/requirejs-mustache/stache',
+		maria: '../../bower_components/maria/maria',
+		crossroads: '../../bower_components/crossroads.js/dist/crossroads'
+	},
+	stache: {
+		extension: '.mustache',
+		path: '../templates/'
+	}
+});
+
+require([
+	'stache!hello'
+], function (helloTemplate) {
+	document.body.innerHTML = helloTemplate({
+		name: 'Wolfy87'
+	});
+});
