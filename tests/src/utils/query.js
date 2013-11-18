a904ce85759b801d2bc2732d8ca3b4c0d61bcd5d
Oliver Caldwell <olliec87@gmail.com>
2013-11-18 23:28:07 +0000

    Query now stores it's element in `this`.

--- tests/src/utils/query.js
+++ tests/src/utils/query.js
@@ -2,23 +2,22 @@ define([
 	'utils/query'
 ], function (query) {
 	suite('utils/query', function () {
-		var testElement;
-
 		setup(function () {
-			testElement = document.createElement('div');
-			testElement.id = 'some-id';
-			testElement.classList.add('some-class');
-			document.body.appendChild(testElement);
+			this.testElement = document.createElement('div');
+			this.testElement.id = 'some-id';
+			this.testElement.classList.add('some-class');
+			document.body.appendChild(this.testElement);
 		});
 
 		teardown(function () {
-			document.body.removeChild(testElement);
+			document.body.removeChild(this.testElement);
 		});
 
 		suite('()', function () {
 			test('can find a single element by selector', function () {
 				var match = query('div.some-class');
 				assert.instanceOf(match, HTMLElement, 'the match was a HTML element');
+				assert.strictEqual(match, this.testElement, 'the match is the correct element');
 			});
 		});
 
@@ -26,6 +25,7 @@ define([
 			test('can find an element by ID', function () {
 				var match = query.id('some-id');
 				assert.instanceOf(match, HTMLElement, 'the match was a HTML element');
+				assert.strictEqual(match, this.testElement, 'the match is the correct element');
 			})
 		});
 
@@ -34,6 +34,7 @@ define([
 				var matches = query.all('div.some-class');
 				assert.instanceOf(matches, NodeList, 'the search returned a NodeList');
 				assert.lengthOf(matches, 1, 'the NodeList contained one item');
+				assert.strictEqual(matches[0], this.testElement, 'the match is the correct element');
 			});
 		});
 	});
