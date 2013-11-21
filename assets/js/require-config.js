0fc916f1263763ecfc08219f774aeb21d62669a0
Oliver Caldwell <olliec87@gmail.com>
2013-11-21 20:55:00 +0000

    Wrapped config in define and main in require.

--- assets/js/require-config.js
+++ assets/js/require-config.js
@@ -1,19 +1,19 @@
-require.config({
-	paths: {
-		mustache: '../../bower_components/mustache/mustache',
-		text: '../../bower_components/requirejs-text/text',
-		stache: '../../bower_components/requirejs-mustache/stache',
-		stapes: '../../bower_components/stapes/stapes',
-		crossroads: '../../bower_components/crossroads.js/dist/crossroads',
-		hasher: '../../bower_components/hasher/dist/js/hasher',
-		signals: '../../bower_components/js-signals/dist/signals',
-		qwery: '../../bower_components/qwery/qwery',
-		lodash: '../../bower_components/lodash/dist/lodash'
-	},
-	stache: {
-		extension: '.mustache',
-		path: '../templates/'
-	}
+define(function () {
+	require.config({
+		paths: {
+			mustache: '../../bower_components/mustache/mustache',
+			text: '../../bower_components/requirejs-text/text',
+			stache: '../../bower_components/requirejs-mustache/stache',
+			stapes: '../../bower_components/stapes/stapes',
+			crossroads: '../../bower_components/crossroads.js/dist/crossroads',
+			hasher: '../../bower_components/hasher/dist/js/hasher',
+			signals: '../../bower_components/js-signals/dist/signals',
+			qwery: '../../bower_components/qwery/qwery',
+			lodash: '../../bower_components/lodash/dist/lodash'
+		},
+		stache: {
+			extension: '.mustache',
+			path: '../templates/'
+		}
+	});
 });
-
-define();
