823aa632a5e9bc68d43ff5b4ddc4fa0623721738
Oliver Caldwell <olliec87@gmail.com>
2013-11-22 23:56:04 +0000

    Added PageRouter binding and setting with tests.
    Should now be able to hook in a render and a transition-er?

--- assets/js/trillek/PageRouter.js
+++ assets/js/trillek/PageRouter.js
@@ -19,6 +19,40 @@ define([
 			hasher.initialized.add(parseHash);
 			hasher.changed.add(parseHash);
 			hasher.init();
+		},
+
+		/**
+		 * Binds the page's route.matched signal to setCurrentPage and the
+		 * routeMatched event. After binding a page it will become the current
+		 * page when it's route is matched.
+		 *
+		 * @param {Function} Page
+		 */
+		bindPage: function (Page) {
+			var route = Page.route;
+
+			route.matched.add(function () {
+				this.emit('routeMatched', route);
+				this.setCurrentPage(Page);
+			}.bind(this));
+		},
+
+		/**
+		 * Replaces the current page by instantiating the page you provide. It
+		 * will also emit a pageSet event. This event's payload contains a `to`
+		 * property, the page you just moved to, and possibly a `from`
+		 * property, the page you are moving from.
+		 *
+		 * @param {Function} Page
+		 */
+		setCurrentPage: function (Page) {
+			var previousPage = this._currentPageInstance;
+			var currentPage = this._currentPageInstance = new Page();
+
+			this.emit('pageSet', {
+				to: currentPage,
+				from: previousPage
+			});
 		}
 	});
 
--- tests/tests/PageRouter.js
+++ tests/tests/PageRouter.js
@@ -1,9 +1,77 @@
 define([
-	'trillek/PageRouter'
-], function (PageRouter) {
+	'crossroads',
+	'trillek/PageRouter',
+	'trillek/pages/AbstractPage'
+], function (crossroads, PageRouter, AbstractPage) {
 	suite('PageRouter', function () {
 		setup(function () {
+			this.router = crossroads.create();
 			this.pageRouter = new PageRouter();
+			this.TestPage = AbstractPage.subclass();
+			this.TestPage.extend({
+				route: this.router.addRoute('foo')
+			});
+		});
+
+		suite('#bindPage()', function () {
+			test('when an added page is routed to, it dispatches an event', function () {
+				var routed = false;
+				this.pageRouter.bindPage(this.TestPage);
+				this.pageRouter.on('routeMatched', function () {
+					routed = true;
+				});
+				this.router.parse('foo');
+				assert.isTrue(routed, 'the routed variable was swapped to true when the PageRouter event fired');
+			});
+
+			test('the event that is fired when we have a match passes the route', function () {
+				var routed = false;
+				this.pageRouter.bindPage(this.TestPage);
+				this.pageRouter.on('routeMatched', function (route) {
+					routed = true;
+					assert.strictEqual(route, this.TestPage.route, 'the correct route was passed');
+				}.bind(this));
+				this.router.parse('foo');
+				assert.isTrue(routed, 'the routed variable was swapped to true when the PageRouter event fired');
+			});
+		});
+
+		suite('#setCurrentPage()', function () {
+			test('setting the page instatiates an instance of the page', function () {
+				var initialised = false;
+				var TestPageWithInitCheck = this.TestPage.subclass({
+					constructor: function () {
+						initialised = true;
+					}
+				});
+				this.pageRouter.setCurrentPage(TestPageWithInitCheck);
+				assert.isTrue(initialised, 'the page was initialised when it was set');
+			});
+
+			test('setting a page emits the pageSet event with the to property', function () {
+				var emitted = false;
+				this.pageRouter.on('pageSet', function (payload) {
+					emitted = true;
+					assert.instanceOf(payload.to, this.TestPage, 'the to property is a TestPage instance');
+				}.bind(this));
+				this.pageRouter.setCurrentPage(this.TestPage);
+				assert.isTrue(emitted, 'the event was emitted and set the flag');
+			});
+
+			test('setting a page emits the pageSet event with the from property', function () {
+				var emitted = false;
+				var OtherTestPage = AbstractPage.subclass();
+
+				this.pageRouter.setCurrentPage(this.TestPage);
+				this.pageRouter.on('pageSet', function (payload) {
+					emitted = true;
+					assert.instanceOf(payload.to, OtherTestPage, 'the to property is a OtherTestPage instance');
+					assert.instanceOf(payload.from, this.TestPage, 'the from property is a TestPage instance');
+				}.bind(this));
+				this.pageRouter.setCurrentPage(OtherTestPage);
+
+				assert.isTrue(emitted, 'the event was emitted and set the flag');
+			});
 		});
 	});
 });
