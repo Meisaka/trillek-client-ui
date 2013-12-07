e465ae573b663c9e8e6a02c2be6ff5a608812f61
Oliver Caldwell <olliec87@gmail.com>
2013-12-07 17:46:53 +0000

    Added a mapping through to the play function.

--- assets/js/trillek/services/GameBridge.js
+++ assets/js/trillek/services/GameBridge.js
@@ -8,7 +8,24 @@ define([
 	 * @class
 	 */
 	var GameBridge = Stapes.subclass(/** @lends GameBridge.prototype */ {
+		/**
+		 * @constructs
+		 */
+		constructor: function () {
+			this._gameObject = window.game;
+		},
+
+		/**
+		 * @return {Boolean}
+		 */
+		isGameObjectPresent: function () {
+			return typeof this._gameObject === 'object';
+		},
+
 		play: function () {
+			if (this.isGameObjectPresent()) {
+				this._gameObject.play();
+			}
 		}
 	});
 
