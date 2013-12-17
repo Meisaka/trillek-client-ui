9300020e34e5cf64d5871adc1ff6e9dee40b1542
Oliver Caldwell <olliec87@gmail.com>
2013-12-17 16:01:36 +0000

    Tests for HomePageController.

--- tests/tests/controllers/HomePageController.js
+++ tests/tests/controllers/HomePageController.js
@@ -5,5 +5,33 @@ define([
 		setup(function () {
 			this.controller = new HomePageController();
 		});
+
+		suite('#onPlayClicked()', function () {
+			setup(function () {
+				this.playClickedSpy = sinon.spy();
+				this.controller.on('playClicked', this.playClickedSpy);
+
+				this.playSpy = sinon.spy();
+				window.trillek = {
+					gameBridge: {
+						play: this.playSpy
+					}
+				};
+			});
+
+			teardown(function () {
+				delete window.trillek;
+			});
+
+			test('calls play on the game bridge', function () {
+				this.controller.onPlayClicked();
+				assert.isTrue(this.playSpy.called);
+			});
+
+			test('emits playClicked', function () {
+				this.controller.onPlayClicked();
+				assert.isTrue(this.playClickedSpy.called);
+			});
+		});
 	});
 });
