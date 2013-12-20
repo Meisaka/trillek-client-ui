dc85c5bb098193e9615580fc23d22e67eb934370
Oliver Caldwell <olliec87@gmail.com>
2013-12-20 11:14:17 +0000

    Replaced the ONLY for loop with _.forEach.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,8 @@
 require([
+	'lodash',
 	'trillek/services',
 	'trillek/pages/all'
-], function (services, allPages) {
-	var allPagesLength = allPages.length;
-	var i;
-
-	for (i = 0; i < allPagesLength; i++) {
-		services.pageRouter.bindPage(allPages[i]);
-	}
-
+], function (_, services, allPages) {
+	_.forEach(allPages, services.pageRouter.bindPage);
 	services.pageRouter.initialiseHasher();
 });
