37228d87806e35bb350f016107ef59543784a519
Oliver Caldwell <olliec87@gmail.com>
2013-12-20 11:20:01 +0000

    Corrected the scope of the main page loop.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,6 @@ require([
 	'trillek/services',
 	'trillek/pages/all'
 ], function (_, services, allPages) {
-	_.forEach(allPages, services.pageRouter.bindPage);
+	_.forEach(allPages, services.pageRouter.bindPage, services.pageRouter);
 	services.pageRouter.initialiseHasher();
 });
