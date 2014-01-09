b77362bdd54a53efbcfebf21758b35f92f7debc3
Oliver Caldwell <olliec87@gmail.com>
2014-01-09 22:42:40 +0000

    Very basic hello world. Angular is loaded.

--- assets/js/require-config.js
+++ assets/js/require-config.js
@@ -1,6 +1,12 @@
 require.config({
 	baseUrl: './assets/js',
 	paths: {
-		almond: '../../bower_components/almond/almond'
+		almond: '../../bower_components/almond/almond',
+		angular: '../../bower_components/angular/angular'
+	},
+	shim: {
+		angular: {
+			exports: 'angular'
+		}
 	}
 });
