056bc58b811a5738e3c9e570d2d63c5b9fba2a61
Oliver Caldwell <olliec87@gmail.com>
2014-02-15 14:01:38 +0000

    Tested and filled out PlayController.

--- src/assets/js/trillek/controllers/PlayController.js
+++ src/assets/js/trillek/controllers/PlayController.js
@@ -3,11 +3,21 @@ define(function () {
 
 	/**
 	 * @class PlayController
+	 * @param {Object} $scope
+	 * @param {Object} gameBridge
+	 * @param {Object} $location
+	 * @param {Object} config
 	 */
-	function PlayController() {
+	function PlayController($scope, gameBridge, $location, config) {
+		$scope.resume = gameBridge.resume;
+		$scope.quit = gameBridge.quit;
+		$scope.stop = function () {
+			gameBridge.stop();
+			$location.path(config.routes.mainMenu);
+		};
 	}
 
-	PlayController.$inject = [];
+	PlayController.$inject = ['$scope', 'gameBridge', '$location', 'config'];
 
 	return PlayController;
 });
--- tests/specs/controllers/PlayController.spec.js
+++ tests/specs/controllers/PlayController.spec.js
@@ -5,9 +5,51 @@ define(function (require) {
 
 	describe('trillek/controllers/PlayController', function () {
 		var controller;
+		var scope;
+		var bridge;
+		var location;
+		var config;
 
 		beforeEach(function () {
-			controller = new PlayController();
+			scope = {};
+			location = {
+				path: jasmine.createSpy('path')
+			};
+			config = {
+				routes: {
+					mainMenu: 'MAIN_MENU_ROUTE'
+				}
+			};
+			bridge = {
+				resume: jasmine.createSpy('resume'),
+				stop: jasmine.createSpy('stop'),
+				quit: jasmine.createSpy('quit')
+			};
+			controller = new PlayController(scope, bridge, location, config);
+		});
+
+		it('proxies the resume function to the game', function () {
+			scope.resume();
+			expect(bridge.resume).toHaveBeenCalled();
+		});
+
+		it('proxies the quit function to the game', function () {
+			scope.quit();
+			expect(bridge.quit).toHaveBeenCalled();
+		});
+
+		describe('when stop is called', function () {
+			beforeEach(function () {
+				scope.stop();
+			});
+
+			it('proxies the stop function to the game', function () {
+				expect(bridge.stop).toHaveBeenCalled();
+			});
+
+			it('tries to redirect to the main menu on stop', function () {
+				expect(location.path).toHaveBeenCalledWith(config.routes.mainMenu);
+			});
 		});
 	});
 });
