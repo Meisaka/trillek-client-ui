13b416d5d41fc04051718a69c1f7e819aabdaf53
Valdemar Erk <valdemarerk@gmail>
2014-02-15 20:41:19 +0100

    corrected an error in da.js

--- src/assets/js/trillek/i18n/da.js
+++ src/assets/js/trillek/i18n/da.js
@@ -1,6 +1,6 @@
 define({
 	PLAY_GAME: 'spil',
-	RESUME_GAME: 'genoptag',
+	RESUME_GAME: 'genoptag spil',
 	RETURN_TO_MENU: 'tilbage til menuen',
 	QUIT_GAME: 'afslut spil',
 	SELECT_LANGUAGE: 'vælg sprog'
